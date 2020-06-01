6594e8c6b537e90808cdbe45e265df2c68ac6320
hwon22 <s2018s06@e-mirim.hs.kr>
2020-06-01 09:12:30 +0900

    Initial commit

---
+++ app/src/main/java/com/example/dungdung/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.dungdung;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //상태바 없애기 ▽
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+    }
+}
