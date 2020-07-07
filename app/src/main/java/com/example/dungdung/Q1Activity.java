b08d4822e7d1e5fcdde5aa5b6c946f1e581d7544
Hwowon <s2018s06@e-mirim.hs.kr>
2020-07-07 15:50:45 +0900

    merge 수정

--- app/src/main/java/com/example/dungdung/Q1Activity.java
+++ app/src/main/java/com/example/dungdung/Q1Activity.java
@@ -142,8 +142,8 @@ public class Q1Activity extends Activity {
                     quiz_r[3]='p';
                 }
                 String str = String.valueOf(quiz_r);
-                showToast(Q1Activity.this, "당신에게 꼭 맞는 독서법 입니다!"+str);
-                if(str==mbti[0]){
+                showToast(Q1Activity.this, str+"형인 당신에게 꼭 맞는 독서법 입니다!");
+                if(str.equals(mbti[0])){
                     myStartActivity(QR1Activity.class);
                 }
             }
